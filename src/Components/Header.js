941fcfbbf55b010a8d4c6fbf9b48c7b039cf612c
KhokharYugYug <83321546+kyr1234@users.noreply.github.com>
2021-08-29 05:56:45 +0000

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Header() {
+  return (
+    <div className="Header">
+<MenuIcon/>
+    </div>
+  );
+}
+
+export default Header;
