2dca4ca6fcf82bb47b1d56b9158f96974b0567a4
Yug KHOKHAR <83321546+kyr1234@users.noreply.github.com>
2021-08-31 07:23:44 +0000

    react

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import Videos from './Components/Video';
-
+import {Router,Switch,Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
+<div className="App">
+<Router>
   <Header/>
-  
+
+<Switch>
+<Route path='/search/:searhTerm'>
+
+
+
+  <Route path='/'>
   <div className='mainpage'>
   <Sidebar/>
   <Videos/>
-  </div>   
-     
-    
-</div>
+  </div> 
+  </Route>  
+</Switch>     
 
+</Router>    
+</div>
   );
 }
 
